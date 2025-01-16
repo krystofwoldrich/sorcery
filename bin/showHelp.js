b4ad37a8173836615c9d3d47d8217fb385629b14
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-01-16 16:43:19 +0100

    fix: Update help command outdated cjs

--- bin/showHelp.js
+++ bin/showHelp.js
@@ -1,5 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function (stream) {
 	fs.readFile(path.join(__dirname, 'help.md'), function (err, result) {
@@ -9,7 +13,7 @@ export default function (stream) {
 
 		help = result
 			.toString()
-			.replace('<%= version %>', require('../package.json').version);
+			.replace('<%= version %>', JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url))).version);
 		(stream || process.stderr).write('\n' + help + '\n');
 	});
 }
