0d0256d21aa9a851367ef39bf063b46d77b197cd
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-01-16 16:42:32 +0100

    fix: Log warning on non-existing source maps

--- src/utils/getMap.js
+++ src/utils/getMap.js
@@ -13,6 +13,13 @@ export default function getMap(node, sourceMapByPath, sync) {
 			return sync ? null : Promise.resolve(null);
 		}
 
-		return getMapFromUrl(url, node.file, sync);
+		try {
+			return getMapFromUrl(url, node.file, sync);
+		} catch (error) {
+			console.error(`Error getting source map for ${node.file}: ${error.message}`);
+			// not correct, but if the source maps don't ship with the library we can treat the file as original source
+			node.isOriginalSource = true;
+			return sync ? null : Promise.resolve(null);
+		}
 	}
 }
