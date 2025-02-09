b4a9dea22c7d57270b3604c20add748cfe600125
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-02-09 19:08:21 +0100

    fix(cli): Help command was using outdated cjs syntax
    Squashed commit of the following:
    
    commit 39495f7c762380c1b3026922faf4e1b6922a3c7d
    Author: Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
    Date:   Thu Jan 16 14:43:22 2025 +0100
    
        fix(cli): Help command was using outdated cjs syntax

--- bin/showHelp.js
+++ bin/showHelp.js
@@ -1,5 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function (stream) {
 	fs.readFile(path.join(__dirname, 'help.md'), function (err, result) {
@@ -7,9 +11,13 @@ export default function (stream) {
 
 		if (err) throw err;
 
-		help = result
-			.toString()
-			.replace('<%= version %>', require('../package.json').version);
+		help = result.toString().replace('<%= version %>', getVersion());
 		(stream || process.stderr).write('\n' + help + '\n');
 	});
+}
+
+export function getVersion() {
+	return JSON.parse(
+		fs.readFileSync(new URL('../package.json', import.meta.url))
+	).version;
 }
