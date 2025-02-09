115b1d9b83b4582398528b6a4988d4581a7784a4
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-02-09 19:07:25 +0100

    feat: Allow missing source maps
    Squashed commit of the following:
    
    commit a4440a3ae60adb917a08eb589d2ff9e728742971
    Author: Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
    Date:   Thu Jan 16 16:12:03 2025 +0100
    
        feat: Allow missing source maps

--- src/utils/getMap.js
+++ src/utils/getMap.js
@@ -13,6 +13,30 @@ export default function getMap(node, sourceMapByPath, sync) {
 			return sync ? null : Promise.resolve(null);
 		}
 
-		return getMapFromUrl(url, node.file, sync);
+		let map = null;
+		try {
+			map = getMapFromUrl(url, node.file, sync);
+		} catch (error) {
+			handleError(node, error);
+		}
+
+		if (!map) {
+			return sync ? null : Promise.resolve(null);
+		}
+
+		if (sync) {
+			return map;
+		} else {
+			return map.catch((reason) => {
+				handleError(node, reason);
+				return null;
+			});
+		}
 	}
+}
+
+function handleError(node, error) {
+	// not correct, but if the source maps don't ship with the library we can treat the file as original source
+	node.isOriginalSource = true;
+	console.error(`Error getting source map for ${node.file}: ${error.message}`);
 }
